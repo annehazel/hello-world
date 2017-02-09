fc387230389dc467245ca772e464c820d4f6b1d2
Anne Hazel <anne.hazel2@gmail.com>
2017-02-09 15:04:50 -0500

    fixed image name, added link

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <h1>Hello World</h1>
 
-<img src='/images/fernanda.job' alt='picture-of-fernanda'>
+<img src="/images/fernanda.jpg" alt="picture-of-fernanda" height="80" width="130"/> <a href="https://en.wikipedia.org/wiki/Kitten">Learn about kittens on Wikipedia</a>
